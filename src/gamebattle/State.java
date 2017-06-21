14101b96b4f7e071d641e7e519834b6acbf3ee07
javajunior2 <tracker28@i.ua>
2017-06-21 19:40:40 +0300

    added class State

---
+++ src/gamebattle/State.java
@@ -0,0 +1,24 @@
+package gamebattle;
+
+public class State {
+
+    private int healthPoints;
+    private int damage;
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(int hp) {
+        this.healthPoints = hp;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int dmg) {
+        this.damage = dmg;
+    }
+
+}
